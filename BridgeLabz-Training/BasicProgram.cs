ed2bed17cb2d20975c8f81e80d9de0655851a498
Vipin-Attri <vipinattri9211420@gmail.com>
2026-04-17 13:03:33 +0530

    basic program

---
+++ BridgeLabz-Training/BasicProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training
+{
+    internal class BasicProgram
+    {
+        public void BasicProgramFun() { 
+        
+            string name = "Vipin";
+
+            Console.WriteLine(name);
+
+            string ss = "";
+
+
+            for(int i=name.Length-1; i>=0; i--)
+            {
+                ss = ss + name[i];
+            }
+
+
+            Console.WriteLine(ss);
+        }
+    }
+}
---
+++ BridgeLabz-Training/MainFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training
+{
+    internal class MainFile
+    {
+        public static void Main(string[] args)
+        {
+
+            BasicProgram bp = new BasicProgram();
+
+            Console.WriteLine("string reverse!");
+            bp.BasicProgramFun();
+           
+        }
+    }
+}
