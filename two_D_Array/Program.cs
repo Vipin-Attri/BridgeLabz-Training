577c12bff1874ebed4196a0d4c49e6500f53f087
Vipin-Attri <vipinattri9211420@gmail.com>
2026-04-22 10:56:31 +0530

    Sum of all the elements of a 2D-Array

---
+++ two_D_Array/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int row = 3, col = 3;
+int[,] array = new int[row,col];
+int sum = 0;
+for(int i=0; i < row; i++)
+{
+    for(int j = 0; j < col; j++)
+    {
+        Console.Write("Enter the value for array[" + i + "," + j + "]: ");
+        array[i, j] = Convert.ToInt32(Console.ReadLine());
+        sum += array[i, j];
+    }
+
+}
+Console.WriteLine("The sum of all the elements of the array is:" + sum);
