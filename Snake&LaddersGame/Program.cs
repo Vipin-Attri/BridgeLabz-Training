24523e1fe6be1bde77cac5124f2c5c0f0a8f9371
Vipin-Attri <vipinattri9211420@gmail.com>
2026-04-23 11:19:37 +0530

    Jagged Array

---
+++ Snake&LaddersGame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_LaddersGame
+{
+    internal class Program
+    {
+        static Random random = new Random();
+
+        static int Play(int position)
+        {
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine($"You rolled the dice and got: {diceRoll}");
+
+            int newPosition = position + diceRoll;
+
+            if (newPosition > 100)
+            {
+                position = 100 - (newPosition - 100);
+            }
+            else
+            {
+                position = newPosition;
+            }
+
+            Console.WriteLine($"Your new position is: {position}");
+            return position;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Snake and Ladders Game\n*************************************\n");
+            int player1Position = 0 , player2Position = 0;
+            Console.WriteLine("Player 1 starts at position: " + player1Position);
+            Console.WriteLine("Player 2 starts at position: " + player2Position);
+            Console.WriteLine("First player to reach position 100 wins the game!\n");
+            Console.WriteLine("Press Enter to start the game...");
+            Console.ReadLine();
+            while (player1Position < 100 && player2Position < 100)
+            {
+                               Console.WriteLine("Player 1 Turn");
+                player1Position = Play(player1Position);
+                if (player1Position >= 100)
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                    break;
+                }
+                Console.WriteLine("Player 2 Turn");
+                player2Position = Play(player2Position);
+                if (player2Position >= 100)
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ twoDArrayss/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+using System;
+
+Console.Write("Enter the no. of rows: ");
+
+int[][] arr = new int[Convert.ToInt32(Console.ReadLine())][];
+arr[0] = new int[] { 1, 2 };
+arr[1] = new int[] { 4, 5, 6 };
+arr[2] = new int[] { 7 };
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr[i].Length; j++)
+    {
+        Console.Write(arr[i][j] + " ");
+    }
+    Console.WriteLine();
+}
