63f8c2c5d98753de66f2c28b5f1308d7d648289b
Vipin-Attri <vipinattri9211420@gmail.com>
2026-04-30 14:17:32 +0530

    Generic code

---
+++ GenericLearning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intarray = { 1, 2, 3 };
+            double[] doublearray = { 9.0, 8.0, 7.0 };
+            string[] stringarray = { "55", "44", "33" };
+            DisplayInfo(intarray);
+            DisplayInfo(doublearray);
+            DisplayInfo(stringarray);
+        }
+
+        public static void DisplayInfo<T>(T[] array)
+        {
+            foreach(T item in array)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
