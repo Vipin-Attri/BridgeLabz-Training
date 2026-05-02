26d645989ff560e6375eb8b58a08125237366766
Vipin-Attri <vipinattri9211420@gmail.com>
2026-05-02 06:30:51 +0530

    Invalid Age Exception

---
+++ CustomException/InvalidAgeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    internal class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string message) : base(message) 
+        { }
+    }
+}
---
+++ CustomException/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    internal class Program
+    {
+        static void ValidateAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new InvalidAgeException("Invalid age");
+                }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age;
+                age = Convert.ToInt32(Console.ReadLine());
+                ValidateAge(age);
+                Console.WriteLine("Age is valid");
+            }
+            catch (InvalidAgeException) {
+                Console.WriteLine("Age must be 18 or above");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+
+        }
+    }
+}
