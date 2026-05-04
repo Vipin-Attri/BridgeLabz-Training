9a6815d79c77930f13a6cf06711aef3c1b796bde
Vipin-Attri <vipinattri9211420@gmail.com>
2026-05-05 04:28:42 +0530

    Learning lambda

---
+++ LearningLambda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLambda
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Func<int, int, int> add = (a, b) => a + b;
+            int result = add(1, 2);
+            Console.WriteLine(result);
+            Action<string> print = msg => Console.WriteLine(msg);
+            print("Teen Paanch");
+        }
+    }
+}
