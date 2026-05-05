82cdf33df882d106e7fbc7066a48942ab3b60c44
Vipin-Attri <vipinattri9211420@gmail.com>
2026-05-05 06:10:46 +0530

    Learning events

---
+++ learningEvents/Process.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningEvents
+{
+    public delegate void Notify();
+    internal class Process
+    {
+        public event Notify OnCompleted;
+
+        public void Start()
+        {
+            Console.WriteLine("Process Invoked");
+            OnCompleted?.Invoke();
+        }
+    }
+}
---
+++ learningEvents/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p=new Process();
+            p.OnCompleted += Task1;
+            p.OnCompleted += Task2;
+            p.Start();
+
+
+        }
+        public static void Task1()
+        {
+            Console.WriteLine("Task1 copleted");
+        }
+        public static void Task2()
+        {
+            Console.WriteLine("Task1 copleted");
+        }
+    }
+}
